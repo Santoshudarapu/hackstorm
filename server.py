d6a52a4bec089045741cada4a70d731b41109e83
Santoshudarapu <santoshudharapu7@gmail.com>
2025-03-11 09:32:22 +0530

    Add files via upload

---
+++ server.py
@@ -0,0 +1,12 @@
+from waitress import serve
+from app import app
+
+if __name__ == '__main__':
+    # Configure Waitress server
+    serve(
+        app,
+        host='0.0.0.0',  # Listen on all available network interfaces
+        port=8080,       # Port to run the server on
+        threads=4,       # Number of worker threads
+        url_scheme='http'
+    )
